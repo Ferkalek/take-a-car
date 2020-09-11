09b2538cfa47124573f30c964c1b04dfb8ab2ceb
Ferkalek <ferkalekanton@gmail.com>
2020-09-11 13:58:59 +0200

    settings

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import * as path from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 import { MongooseModule } from "@nestjs/mongoose";
 import { CarsModule } from "./cars/cars.module";
@@ -6,6 +8,12 @@ import { CarsModule } from "./cars/cars.module";
 import config from "@env/config";
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoURL), CarsModule],
+  imports: [
+    MongooseModule.forRoot(config.mongoURL),
+    CarsModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.join("./public"),
+    }),
+  ],
 })
 export class AppModule {}
--- packages/api/src/env/demo/config.ts
+++ packages/api/src/env/demo/config.ts
@@ -1,3 +1,3 @@
 export default {
-    mongoURL: 'mongodb+srv://bdadmin:Rq5jumVWmakYDfaW@cluster0-4fmlh.mongodb.net/takeacar?retryWrites=true&w=majority'
-}
+  mongoURL: `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_PASS}@cluster0-4fmlh.mongodb.net/takeacar?retryWrites=true&w=majority`,
+};
