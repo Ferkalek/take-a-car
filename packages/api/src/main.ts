ef57fd7dea133fb564aa85564b008629a51b20f1
Ferkalek <ferkalekanton@gmail.com>
2020-09-11 14:08:10 +0200

    settup 2

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from "@nestjs/core";
-import * as bodyParser from "body-parser";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -7,6 +6,6 @@ async function bootstrap() {
   // delete these
   // app.use(bodyParser.json({ limit: "50mb" }));
   // app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- packages/ui/src/app/shared/api.const.ts
+++ packages/ui/src/app/shared/api.const.ts
@@ -1,4 +1,4 @@
-import { environment } from "../../environments/environment.prod";
+import { environment } from "../../environments/environment";
 
 const BASE_URL = environment.baseUrl;
 const CARS = `${BASE_URL}/cars`;
